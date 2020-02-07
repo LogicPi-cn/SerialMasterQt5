6778c3690c7224085ba895e4cba3165e998dc1c5
Craftor <craftor@126.com>
2020-02-07 21:31:14 +0800

    add splash

--- main.cpp
+++ main.cpp
@@ -1,12 +1,29 @@
 #include "mainwindow.h"
 #include <QApplication>
-#include <QTextCodec>
+#include <QSplashScreen>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+
+    // Splash Screen On
+    QPixmap pixmap(":/icon/LogicPi.cn.png");
+    QSplashScreen splash(pixmap);
+    splash.show();
+
+    // Delay
+    QDateTime n = QDateTime::currentDateTime();
+    QDateTime now;
+    do {
+        now = QDateTime::currentDateTime();
+    } while (n.secsTo(now) <= 0.5);
+
+    // Main Window
     MainWindow w;
     w.show();
+
+    // Close
+    splash.finish(&w);
 
     return a.exec();
 }
